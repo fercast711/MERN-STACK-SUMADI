de6c5408c23358c63d429feb3618f4fbf153e986
fercast711 <fercast7.fc@gmail.com>
2023-02-22 19:18:31 -0600

    test complete logic and design

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <div className="bg-neutral-800 flex justify-between px-20 py-2  w-screen fixed shadow-md shadow-black">
+    <div className="bg-neutral-800 flex justify-between px-20 sm:px-2 py-2  w-screen fixed shadow-md shadow-black">
         <Link to="https://sumadi.net/es/" target="_blank">
             <img className="w-6" src="https://res.cloudinary.com/dnazobcrv/image/upload/v1677041014/hola/1622753175184_ybe0mn.jpg"/>
         </Link>
