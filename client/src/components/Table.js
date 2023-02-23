e5d7dd9930f9e47f8bad84fc940ec236deaecf98
fercast711 <fercast7.fc@gmail.com>
2023-02-23 00:56:02 -0600

    modified table

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -2,12 +2,12 @@ import {useSelector} from 'react-redux'
 
 const Table = () => {
     const {student} = useSelector(state=> state.student)
-    console.log(student)
     const style_1 = "border-double border-4 border-gray-500 p-2 md:p-0  "
     const style_2 = "border-double border-4 border-gray-500 p-2 md:p-0 w-1/5 sm:w-0 "
   return (
     <div className="w-3/4 md:px-2  md:container flex justify-center">
-    <table className="border-double border-4 border-gray-500  text-gray-400 bg-neutral-800 md:text-xs">
+    <table className="border-double border-4 border-gray-500  text-gray-400  md:text-xs">
+      <thead className='bg-neutral-900  text-gray-300'>
         <tr>
             <th className={style_1}>Student Name</th>
             <th className={style_1}>Date of Birth</th>
@@ -15,15 +15,19 @@ const Table = () => {
             <th className={style_1}>Address</th>
             <th className={style_1}>Gender</th>
         </tr>
-        {student.map(item =>(
+      </thead>
+      <tbody className='bg-neutral-800'>
+      {student.map(item =>(
           <tr>
             <td className={style_2}>{item.FirstName +" "+item.LastName}</td>
             <td className={style_2}>{item.DateBirth}</td>
             <td className={style_2}>{item.Email}</td>
             <td className={style_2}>{item.Address}</td>
-            <td className={style_2}>{item.Gender}</td>
+            <td className={style_2}>{item.Gender}</td> 
           </tr>
         ))}
+      </tbody>
+        
     </table>
     </div>
   )
--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,6 +1,6 @@
 export const NotFound = () => {
   return (
-    <div>
+    <div className="flex items-center justify-center h-screen">
         <h1 className="text-6xl text-center">Not Found</h1>
     </div>
   )
